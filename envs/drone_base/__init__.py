af47f254cb2ed0da81e76f251552cd6d90d2a11a
kousheekc <kousheekc@gmail.com>
2024-12-05 16:17:25 +0100

    sb3 config

--- envs/drone_base/__init__.py
+++ envs/drone_base/__init__.py
@@ -12,6 +12,6 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.drone_base_env_cfg:DroneBaseEnvCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
     },
 )
