aa5e485cc71fd4f32b2a8be13037acdfcba8a291
Kousheek Chakraborty <kousheekc@gmail.com>
2024-12-08 21:12:07 +0100

    body rate NMPC

---
+++ controllers/base_controller.py
@@ -0,0 +1,166 @@
+from abc import ABC, abstractmethod
+
+class BaseController(ABC):
+    """
+    Abstract base class for drone controllers
+    """
+
+    def __init__(self, control_frequency: float, mass: float, moment_of_inertia: dict, thrust_to_weight: float, limit_min: list, limit_max: list, gravity: float):
+        """
+        Initialize the base controller with dynamics and core properties.
+
+        Args:
+            control_frequency (float): Control loop frequency in Hz.
+            mass (float): Drone's mass in kg.
+            moment_of_inertia (dict): Drone's moment of inertia (Ixx, Iyy, Izz).
+            thrust_to_weight (float): Thrust-to-weight ratio of the drone.
+            limit_min (list): Minimum control limit
+            limit_max (list): Maximum control limit
+            gravity (float): Gravity of the environment
+        """
+        # System dynamics properties
+        self._mass = mass
+        self._moment_of_inertia = moment_of_inertia
+        self._thrust_to_weight = thrust_to_weight
+        self._gravity = gravity
+
+        # Control properties
+        self._control_frequency = control_frequency
+        self._limit_min = limit_min
+        self._limit_max = limit_max
+        self._current_state = []
+        self._reference_state = []
+        self._control_output = []
+        self._last_timestamp = 0.0
+
+    ### --- Core Methods --- ###
+    
+    @abstractmethod
+    def set_reference(self, reference_state: list):
+        """Set the desired target state for the drone."""
+        self._reference_state = reference_state
+
+    @abstractmethod
+    def set_current(self, current_state: list):
+        """Set the current state of the drone."""
+        self._current_state = current_state
+
+    @abstractmethod
+    def compute_control(self, timestamp: float|None = None):
+        """
+        Compute control based on the current and target states.
+
+        Args:
+            timestamp (float): Current time for time-dependent calculations.
+
+        Returns:
+            dict: Control outputs (e.g., motor commands, thrust, torques).
+        """
+        pass
+
+    @abstractmethod
+    def reset(self):
+        """Reset the controller's internal state."""
+        pass
+
+    ### --- Properties --- ###
+    
+    @property
+    def mass(self):
+        return self._mass
+
+    @mass.setter
+    def mass(self, value):
+        if value <= 0:
+            raise ValueError("Mass must be positive.")
+        self._mass = value
+
+    @property
+    def moment_of_inertia(self):
+        return self._moment_of_inertia
+
+    @moment_of_inertia.setter
+    def moment_of_inertia(self, value):
+        if not isinstance(value, dict) or not all(k in value for k in ('Ixx', 'Iyy', 'Izz')):
+            raise ValueError("Moment of inertia must be a dictionary with keys: 'Ixx', 'Iyy', 'Izz'.")
+        self._moment_of_inertia = value
+
+    @property
+    def thrust_to_weight(self):
+        return self._thrust_to_weight
+
+    @thrust_to_weight.setter
+    def thrust_to_weight(self, value):
+        if value <= 0:
+            raise ValueError("Thrust-to-weight ratio must be positive.")
+        self._thrust_to_weight = value
+
+    @property
+    def gravity(self):
+        return self._gravity
+
+    @gravity.setter
+    def gravity(self, value):
+        if value <= 0:
+            raise ValueError("Gravity must be positive.")
+        self._gravity = value
+
+    @property
+    def control_frequency(self):
+        return self._control_frequency
+
+    @control_frequency.setter
+    def control_frequency(self, value):
+        if value <= 0:
+            raise ValueError("Control frequency must be positive.")
+        self._control_frequency = value
+
+    @property
+    def limit_min(self):
+        return self._limit_min
+
+    @limit_min.setter
+    def limit_min(self, limit):
+        if not isinstance(limit, list):
+            raise ValueError("Limit must be a list of size output.")
+        self._limit_min = limit
+
+    @property
+    def limit_max(self):
+        return self._limit_max
+
+    @limit_max.setter
+    def limit_max(self, limit):
+        if not isinstance(limit, list):
+            raise ValueError("Limit must be a list of size output.")
+        self._limit_max = limit
+
+    @property
+    def current_state(self):
+        return self._current_state
+
+    @current_state.setter
+    def current_state(self, state):
+        if not isinstance(state, list):
+            raise ValueError("Current state must be a list.")
+        self._current_state = state
+
+    @property
+    def reference_state(self):
+        return self._reference_state
+
+    @reference_state.setter
+    def reference_state(self, state):
+        if not isinstance(state, list):
+            raise ValueError("Reference state must be a list.")
+        self._reference_state = state
+
+    @property
+    def control_output(self):
+        return self._control_output
+
+    @control_output.setter
+    def control_output(self, output):
+        if not isinstance(output, list):
+            raise ValueError("Control output must be a list.")
+        self._control_output = output
---
+++ controllers/nmpc/model/body_rate.py
@@ -0,0 +1,89 @@
+from controllers.base_controller import BaseController
+from casadi import *
+import numpy as np
+
+class NMPCBodyRateController(BaseController):
+    def __init__(self, control_frequency: float, mass: float, moment_of_inertia: dict, thrust_to_weight: float, limit_min: list, limit_max: list, gravity: float, prediction_horizon: int, control_horizon: int):
+        super().__init__(control_frequency, mass, moment_of_inertia, thrust_to_weight, limit_min, limit_max, gravity)
+        
+        assert control_horizon < prediction_horizon - 1, f"Control horizon: {control_horizon} muct be smaller than prediction horizon - 1: {prediction_horizon - 1}"  # This will pass
+
+        self.dt = 1/self._control_frequency
+        self.n_p = prediction_horizon
+        self.n_u = control_horizon
+
+        self.opti = Opti()
+
+        self.x = self.opti.variable(10, self.n_p+1) # state trajectory
+        self.u = self.opti.variable(4, self.n_p)   # control trajectory
+
+        self.x_ref = self.opti.parameter(10, self.n_p+1)  # Reference trajectory
+        self.u_ref = self.opti.parameter(4, self.n_p) # Reference controls
+
+        f = lambda x,u: vertcat(
+            x[7],
+            x[8],
+            x[9],
+            0.5 * ( -u[1]*x[4] - u[2]*x[5] - u[3]*x[6] ),
+            0.5 * (  u[1]*x[3] + u[3]*x[5] - u[2]*x[6] ),
+            0.5 * (  u[2]*x[3] - u[3]*x[4] + u[1]*x[6] ),
+            0.5 * (  u[3]*x[3] + u[2]*x[4] - u[1]*x[5] ),
+            2 * ( x[3]*x[5] + x[4]*x[6] ) * u[0],
+            2 * ( x[5]*x[6] - x[3]*x[4] ) * u[0], 
+            (x[3]*x[3] - x[4]*x[4] -x[5]*x[5] + x[6]*x[6]) * u[0] - self.gravity
+        ) # dx/dt = f(x,u)
+
+        for k in range(self.n_p):
+            # Runge-Kutta 4 integration
+            # Discretise
+            k1 = f(self.x[:,k],         self.u[:,k])
+            k2 = f(self.x[:,k]+self.dt/2*k1, self.u[:,k])
+            k3 = f(self.x[:,k]+self.dt/2*k2, self.u[:,k])
+            k4 = f(self.x[:,k]+self.dt*k3,   self.u[:,k])
+            x_next = self.x[:,k] + self.dt/6*(k1+2*k2+2*k3+k4) 
+            self.opti.subject_to(self.x[:,k+1]==x_next) # ensure dynamics
+
+        self.opti.subject_to(self.opti.bounded(self.limit_min[0], self.u[0, :], self.limit_max[0])) # control is limited
+        self.opti.subject_to(self.opti.bounded(self.limit_min[1], self.u[1, :], self.limit_max[1])) # angular velocity is limited
+        self.opti.subject_to(self.opti.bounded(self.limit_min[2], self.u[2, :], self.limit_max[2])) # angular velocity is limited
+        self.opti.subject_to(self.opti.bounded(self.limit_min[3], self.u[3, :], self.limit_max[3])) # angular velocity is limited
+        self.opti.subject_to(self.x[:,0] == [1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])     # Initial state
+        self.opti.subject_to(self.u[:,0] == [9.81, 0.0, 0.0, 0.0])                                  # Initial state
+        
+        x_weight = diag(MX([100, 100, 100, 10, 10, 10, 10, 10, 10, 10]))  # Weights for state error
+        u_weight = diag(MX([0.1, 0.1, 0.1, 0.1]))                         # Weights for control effort
+
+        # Cost function calculations
+        cost = 0 # Cost function init
+        for k in range(self.n_p):
+            state_error = self.x[:, k] - self.x_ref[:, k]
+            cost += mtimes(state_error.T, mtimes(x_weight, state_error))
+            control_error = self.u[:, k] - self.u_ref[:, k]
+            cost += mtimes(control_error.T, mtimes(u_weight, control_error))
+        self.opti.minimize(cost)
+
+        # reference controls (dont change)
+        reference_controls = np.tile(np.array([9.81, 0.0, 0.0, 0.0]), (self.n_p, 1)).T
+        self.opti.set_value(self.u_ref, reference_controls)
+
+        self.opti.solver("ipopt") # set numerical backend
+
+    def set_current(self, current_state):
+        super().set_current(current_state)
+        self.opti.subject_to(self.x[:,0] == current_state)     # Current state
+
+    def set_reference(self, reference_state):
+        super().set_reference(reference_state)
+        self.opti.set_value(self.x_ref, self.reference_state)
+
+    def compute_control(self, timestamp = None):
+        self.opti.set_initial(self.x, 0.0)  # Initial guess
+        sol = self.opti.solve()             # actual solve
+
+        self.opti.subject_to(self.u[:,0] == sol.value(self.u[:, self.n_u-1]))
+        self.control_output = sol.value(self.u[:, self.n_u-1])
+
+        return sol.value(self.u[:, 0:self.n_u])
+
+
+        
